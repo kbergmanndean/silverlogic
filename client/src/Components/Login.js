80fae38ff98832c9411e4b89b951682c52e47444
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-01-05 17:38:29 -0500

    made login component front end

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,6 +1,55 @@
+import {useState} from "react"
+
 function Login({user, setUser}) {
+    const [username, setUserName] = useState("")
+    const [password, setPassword] = useState("")
+    const [errors, setErrors] = useState([])
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        const user = {
+            user_name:username,
+            password:password
+        }
+        const res = await fetch("http://localhost:3000/login",{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify({user})
+        })
+        const userData = await res.json();
+        if (userData.id) {
+            setUser(userData);
+            console.log(user)
+        }else{
+            setErrors(userData.errors)
+            console.log(errors)
+        }
+
+    }
+
+
     return (
-        <h1>Login</h1>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <h1>Log In</h1>
+                <input
+                    type="text"
+                    placeholder="username"
+                    value={username}
+                    name="username"
+                    onChange={(e)=>setUserName(e.target.value)}>
+                </input>
+                <input
+                    type="text"
+                    placeholder="password"
+                    value={password}
+                    name="password"
+                    onChange={(e)=>setPassword(e.target.value)}>
+                </input>
+                <input type="submit" value="Log In"></input>
+            </form>
+            {errors? errors.map(error=><div key={error}>{error}</div>):null}
+        </div>
     )
 }
 
