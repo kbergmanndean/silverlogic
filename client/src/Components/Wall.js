9d0ee283176585a298e868a74fa226f7c0ad9687
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-01-07 13:03:00 -0500

    created user state

--- client/src/Components/Wall.js
+++ client/src/Components/Wall.js
@@ -2,13 +2,12 @@ import {useState} from "react"
 
 function Wall({messages, setMessages}) {
     const [messageText, setMessageText] = useState("")
-
-    const user_id = localStorage.getItem("user_id")
+    const [user, setUser] = useState(localStorage.getItem("user_id"))
 
     async function postMessage(e) {
         e.preventDefault();
         const post = {
-            user_id: user_id,
+            user_id: user,
             text: messageText
         }
         const res = await fetch("http://localhost:3000/posts",{
@@ -22,39 +21,43 @@ function Wall({messages, setMessages}) {
         setMessageText("")
     }
 
-    async function logOut() {
-        const res = await fetch("http://localhost:3000/logout",{
+    function logOut() {
+        async function handleLogOut() {
+            const res = await fetch("http://localhost:3000/logout",{
             method:"DELETE"
         })
-        if (res.ok) {
-            localStorage.clear()
+            if (res.ok) {
+                localStorage.clear()
+                setUser("")
+            }
         }
+        handleLogOut()
     }
 
     return(
         <div>
             <h1>Home</h1>
-            {user_id ?
-            <button onClick={logOut}>Log Out</button>
+            {user ?
+            <div>
+                <button onClick={logOut}>Log Out</button>
+                <form onSubmit = {postMessage}>
+                    <input 
+                        type="text"
+                        placeholder="message"
+                        value={messageText}
+                        onChange={(e)=>setMessageText(e.target.value)}>
+                    </input>
+                </form>
+            </div>
             : 
             <a href="/log-in-auth">Sign Up/Log In</a>}
-            {user_id?
-            <form onSubmit = {postMessage}>
-                <input 
-                    type="text"
-                    placeholder="message"
-                    value={messageText}
-                    onChange={(e)=>setMessageText(e.target.value)}>
-                </input>
-            </form>
-            :null}
             {messages?
             <ul>
             {messages.map(message=>{return <li key={message.id}><p>{message.text}</p><p>({message.user.user_name})</p></li>})}
             </ul>
             :null}
         </div>
-        )
+    )
 }
 
 export default Wall
