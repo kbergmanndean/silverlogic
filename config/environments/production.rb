cb9240fd4bad7218384cc0aea2a69bc28b125533
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-01-08 16:08:47 -0500

    configured app to send welcome email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,22 +117,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  
-  
-  # config.action_mailer.perform_deliveries = true 
-  # config.action_mailer.raise_delivery_errors = true
-  # # config.action_mailer.delivery_method = :smtp
-  # ActionMailer::Base.delivery_method = :sendmail
-  # host = 'localhost:3000'
-  # config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
-  # ActionMailer::Base.sendmail_settings = {
-  # # config.action_mailer.smtp_settings = { 
-  #     :address  => "smtp@gmail.com",
-  #     :port => 587,
-  #     :user_name  => "testwallapp99",
-  #     :password => "testing99",
-  #     :authentication => "plain",
-  #     :enable_starttls_auto => true
-  # }
 
 end
